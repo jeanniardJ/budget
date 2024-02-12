c56f51cf13e6fbe92b6d39787bbf35013ca99e98
Jonathan J <contact@jja-dev.fr>
2024-02-12 22:37:56 +0100

    init projet

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+import Menu from './components/Menu';
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <Menu />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/add-expense">Ajouter une dépense</a></li>
+        <li><a href="/expense-history">Historique des dépenses</a></li>
+        <li><a href="/settings">Paramètres</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
